672c0e5ab6bb2b48e8d275495a1662c82e2713a0
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-08-20 00:18:37 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HistoriaComponent } from './enrrutamiento/historia/historia.component';
+import { HerramientasComponent } from './enrrutamiento/herramientas/herramientas.component';
+import { TecnologiasComponent } from './enrrutamiento/tecnologias/tecnologias.component';
+import { TiposComponent } from './enrrutamiento/tipos/tipos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        //Componente que se mostrara por defecto
+        path: '',
+        component: HistoriaComponent
+    },
+    {
+        path: 'historia',  
+        component: HistoriaComponent
+    },
+    {
+        path: 'herramientas',
+        component: HerramientasComponent
+    },
+    {
+        path: 'tecnologias',
+        component: TecnologiasComponent
+    },
+    {
+        path: 'tipos',
+        component: TiposComponent
+    }
+];
---
+++ src/app/enrrutamiento/tipos/tipos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipos',
+  standalone: true,
+  imports: [],
+  templateUrl: './tipos.component.html',
+  styleUrl: './tipos.component.css'
+})
+export class TiposComponent {
+
+}
